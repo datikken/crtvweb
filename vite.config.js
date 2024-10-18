4f182182e0c97c34483caf3dfdf30456b54239b8
datikken <tikken23@gmail.com>
2024-10-18 22:29:07 +0300

    template started

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,25 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/animate.css',
+                'resources/css/aos.css',
+                'resources/css/bootstrap.min.css',
+                'resources/css/bootstrap-datepicker.css',
+                'resources/css/flaticon.css',
+                'resources/css/icomoon.css',
+                'resources/css/ionicons.min.css',
+                'resources/css/jquery.timepicker.css',
+                'resources/css/magnific-popup.css',
+                'resources/css/open-iconic-bootstrap.min.css',
+                'resources/css/owl.carousel.min.css',
+                'resources/css/owl.theme.default.min.css',
+                'resources/css/style.css',
+                'resources/js/bootstrap.js',
+                'resources/js/google-map.js',
+                'resources/js/main.js',
+                'resources/js/range.js',
+            ],
             refresh: true,
         }),
     ],
